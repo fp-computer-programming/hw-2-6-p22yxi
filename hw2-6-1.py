a0336865c1fa3de4e79e973d6abb746879501575
p22yxi <p22yxi@fairfieldprep.org>
2021-09-26 15:56:44 -0700

    hw

---
+++ hw2-6-1.py
@@ -0,0 +1,6 @@
+# Author : Yongdong Xi (Sep 22 2021)
+
+fahrenheit = float(input("Enter Fahrenheit temperature:"))
+ 
+celsius = (fahrenheit - 32 ) * 5 / 9
+print(fahrenheit)
--- hw2-6.py
+++
@@ -1,6 +0,0 @@
-# Author : Yongdong Xi (Sep 22 2021)
-
-celsius = float(input("Enter Celsius temperature:"))
- 
-fahrenheit = (celsius * 1.8) + 32
-print(fahrenheit)
